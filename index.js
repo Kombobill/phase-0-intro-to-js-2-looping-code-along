28e33eaa183b2264e0a0cfd9ee94ae12badf7c13
Billgates <billmadru25@gmail.com>
2022-11-26 14:25:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+// Code your solution here
 // Code your solutions in this file
+function writeCards (string, event){
+    var thisarray = []; 
+    for (let counter = 0; counter < string.length; counter++){ 
+        thisarray.push(`Thank you, ${string[counter]}, for the wonderful ${event} gift!`);
+      }
+    return thisarray;
+  }
+  function countDown(num){
+    
+    while (num >= 0)
+    {
+        console.log(num)
+        num--;
+    }
+  }
+  
+
